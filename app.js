61f0f07ef01ca64cedf544bde9e95f9b3334226a
Haxorsprogramming <haxors@uinsu.ac.id>
2022-06-26 22:06:33 +0700

    'update'

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const jwt = require('jsonwebtoken');
 const postLib = require("./post-lib.js");
 const lib = require("./lib.js");
+var fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(function(req, res, next) {
@@ -33,6 +35,17 @@ app.get("/post/:slug/detail", (req, res) => {
 
 app.post("/post/data/add", (req, res) => {
     postLib.addPostingan(req, res);
+});
+
+app.post("/test/base64", (req, res) => {
+    let fileImg = req.body.img;
+    let base64Image = fileImg.split(';base64,').pop();
+    let kdFile = uuidv4();
+    let filePath = `file/${kdFile}.png`; 
+    fs.writeFile(filePath, base64Image, {encoding: 'base64'}, function(err) {
+        res.sendStatus(200);
+    });
+    
 });
 
 app.listen(port, () => {
